ab77da0a26ac350ec16f2d41ea8b238b82880d11
MohammedAmer <loard.amer.2010@gmail.com>
2023-11-02 16:20:15 -0700

    task 0 import

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,10 +1,5 @@
 #!/usr/bin/python3
-def main():
-    from add_0 import add
-    a = 1
-    b = 2
-    print(f"{a:d} + {b:d} = {add(a, b):d}")
-
-
-
-main()
+from add_0 import add
+a = 1
+b = 2
+print(f"{a:d} + {b:d} = {add(a, b):d}")
