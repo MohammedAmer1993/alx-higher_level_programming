83192986d7a8f092e9cd9deb7da3045e876e6a8c
MohammedAmer <loard.amer.2010@gmail.com>
2023-11-06 08:16:53 -0800

    task 4 python data struct

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -5,5 +5,5 @@ def new_in_list(my_list, idx, element):
             return my_list.copy()
         else:
             list_cp = my_list.copy()
-	    list_cp[idx] = element
+            list_cp[idx] = element
             return list_cp
