0963a38bed4365d06a3d924d1fd2137de5d2eb61
MohammedAmer <loard.amer.2010@gmail.com>
2023-11-06 08:33:41 -0800

    task 5 python data structure

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def no_c(my_string):
     if my_string:
-        string = my_string.copy()
-        for i in range(len(string)):
-            if string[i] == 'c' or string[i] == 'C':
-                string.pop(i)
+        string = ""
+        for i in range(len(my_string)):
+            if my_string[i] != 'c' and my_string[i] != 'C':
+                string += my_string[i]
         return string
