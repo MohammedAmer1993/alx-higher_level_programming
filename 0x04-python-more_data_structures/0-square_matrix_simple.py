cb4aa02e885a7f7997b43054e9de4843ca8a9302
MohammedAmer <loard.amer.2010@gmail.com>
2023-11-08 16:58:02 -0800

    task 10 python dic

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    myMatrix = matrix.copy()
-    for i in range(len(matrix)):
-        myMatrix[i] = [list(map(lambda x: x ** 2, i)) for i in matrix]
-        return (myMatrix)
+    return ([list(map(lambda x: x * x, i)) for i in matrix])
