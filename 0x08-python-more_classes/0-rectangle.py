12749160696e126c2d0723ce133f306032f3df30
Mohammed Amer <loard.amer.2010@gmail.com>
2023-11-28 00:50:29 +0200

    task 0,1,2,3,4,5,6,7,8,9 python more classes

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Module to define a Rectangle class."""
+
+
+class Rectangle:
+    """this is rectangle"""
+    pass
---
+++ 0x08-python-more_classes/README.md
@@ -0,0 +1,1 @@
+this the readme file for project 0x8 more classes in the future I will make better readme file
