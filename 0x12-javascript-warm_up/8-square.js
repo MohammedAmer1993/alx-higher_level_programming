85ab83a9d509ff890a333ce1e1f6cc5acfe18d96
Mohammed Amer <loard.amer.2010@gmail.com>
2024-02-13 01:36:52 +0200

    task 8 javascript frist time

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,1 +1,14 @@
 #!/usr/bin/node
+
+const size = parseInt(process.argv[2]);
+if (isNaN(size)) {
+  console.log('Missing size');
+} else {
+  let block = '';
+  for (let i = 0; i < size; i++) {
+    block = block + 'X';
+  }
+  for (let i = 0; i < size; i++) {
+    console.log(block);
+  }
+}
