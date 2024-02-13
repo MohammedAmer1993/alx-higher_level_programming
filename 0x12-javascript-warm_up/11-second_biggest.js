1662c16e3a85cabd5b795a08398ecc6dab72ba0d
Mohammed Amer <loard.amer.2010@gmail.com>
2024-02-13 03:13:03 +0200

    task 11 javascript frist time

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,10 +2,14 @@
 let x = process.argv[2];
 let y = process.argv[3];
 let tmp = 0;
-
 if (x === undefined || y === undefined) {
   console.log('NaN');
 } else {
+  if (x < y) {
+    tmp = x;
+    x = y;
+    y = tmp;
+  }
   let i = 0;
   while (process.argv[i + 3]) {
     if (x < process.argv[i + 3]) {
