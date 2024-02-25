997b27937c1ec3c1675b16c2a262c2dcc24a99f0
Mohammed Amer <loard.amer.2010@gmail.com>
2024-02-25 06:15:40 +0200

    task 0 proj test driven

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -3,13 +3,15 @@
 
 
 def add_integer(a, b=98):
-    if type(a) != int or type(a) != float:
+    """add two intger numbers
+    Args:
+        a (int): the frist argument
+        b (int): the second number, defaults to 98
+    Return:
+        the sum of two numbers
+    """
+    if type(a) != int and type(a) != float:
         raise TypeError("a must be an integer")
-    if type(b) != int or type(b) != float:
+    if type(b) != int and type(b) != float:
         raise TypeError("b must be an integer")
     return (int(a) + int(b))
-
-
-if __name__ == "__main__":
-    import doctest
-    doctest.testfile("/tests/0-add_integer.txt")
