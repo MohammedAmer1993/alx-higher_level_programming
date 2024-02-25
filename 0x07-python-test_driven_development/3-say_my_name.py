ac29183439afb15adf301d714f0a6fb4a841119b
Mohammed Amer <loard.amer.2010@gmail.com>
2024-02-25 20:46:53 +0200

    task 2 proj test driven

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,2 +1,10 @@
 #!/usr/bin/python3
 """Module for repeating a name all of tests are found in /tests"""
+
+
+def say_my_name(first_name, last_name=""):
+    if type(first_name) is not str:
+        raise TypeError("first_name must be a string")
+    elif type(last_name) is not str:
+        raise TypeError("last_name must be a string")
+    print("My name is {} {}".format(first_name, last_name), end="")
