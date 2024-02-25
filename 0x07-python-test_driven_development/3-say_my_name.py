d5a749306b07beff86a9274f9a141bbd5ea61e9d
Mohammed Amer <loard.amer.2010@gmail.com>
2024-02-25 21:02:45 +0200

    task 2 proj test driven

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -12,11 +12,5 @@ def say_my_name(first_name, last_name=""):
         raise TypeError("first_name must be a string")
     elif type(last_name) is not str:
         raise TypeError("last_name must be a string")
-    if first_name == "" and last_name == "":
-        print("My name is", end="")
-    elif first_name != "" and last_name == "":
-        print("My name is " + first_name, end="")
-    elif first_name == "" and last_name != "":
-        print("My name is " + last_name, end="")
-    else:
-        print("My name is " + first_name + " " + last_name, end="")
+
+    print("My name is " + first_name + " " + last_name, end="")
