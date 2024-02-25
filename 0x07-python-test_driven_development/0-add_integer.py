72a2b701182a3a158763a75d02e58b3e1561aa5b
Mohammed Amer <loard.amer.2010@gmail.com>
2024-02-25 06:31:37 +0200

    task 0 proj test driven

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""Module for adding intgers all of tests are found in /tests"""
+"""Module for adding intgers all of tests are found in /tests
+Test case for this module is in test dirctory
+"""
 
 
 def add_integer(a, b=98):
@@ -7,9 +9,8 @@ def add_integer(a, b=98):
     Args:
         a (int): the frist argument
         b (int): the second number, defaults to 98
-    Return:
-        the sum of two numbers
-    """
+    Return: the sum of two numbers"""
+
     if type(a) is not int and type(a) is not float:
         raise TypeError("a must be an integer")
     if type(b) is not int and type(b) is not float:
