7b8896dcb8b2098711f7c1186d3e319ed3e574e4
Mohammed Amer <loard.amer.2010@gmail.com>
2024-02-25 17:18:27 +0200

    task 1 proj test drivin

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,2 +1,44 @@
 #!/usr/bin/python3
 """Module for matrix divide all of tests are found in /tests"""
+
+
+def matrix_divided(matrix, div):
+    """matrix_divided - divid a matrix by a num
+    Args:
+        matrix (list of list): the matrix
+        div (int): the dividor
+    Return:
+        a new matrix
+    """
+    if type(matrix) is not list:
+        raise TypeError(
+            "matrix must be a matrix (list of lists) of integers/floats")
+    elif type(matrix[0]) is not list:
+        raise TypeError(
+            "matrix must be a matrix (list of lists) of integers/floats")
+    number_of_elemnts = len(matrix[0])
+    for i in matrix:
+        if type(i) is not list:
+            raise TypeError(
+                "matrix must be a matrix (list of lists) of integers/floats")
+        elif len(i) != number_of_elemnts:
+            raise TypeError("each row of the matrix must have the same size")
+    for i in matrix:
+        for m in i:
+            if type(m) is not int and type(m) is not float:
+                raise TypeError(
+                    "matrix must be a matrix"
+                    + "(list of lists) of integers/floats")
+    if type(div) is not int and type(div) is not float:
+        raise TypeError("div must be a number")
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+
+    my_list = []
+    for i in range(len(matrix)):
+        my_list.append([])
+        for index in range(len(matrix[i])):
+            res = round((matrix[i][index] / div), 2)
+            my_list[i].append(res)
+
+    return my_list
