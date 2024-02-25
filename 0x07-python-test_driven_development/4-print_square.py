dee7029dd737556c961f07f2c34a3fb348403596
Mohammed Amer <loard.amer.2010@gmail.com>
2024-02-25 23:52:04 +0200

    task 3 proj test driven

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,2 +1,17 @@
 #!/usr/bin/python3
 """Module for printing square all of tests are found in /tests"""
+
+
+def print_square(size):
+    """print square
+    Args:
+        size (int): the size of square
+    """
+    if type(size) is float or type(size) is not int:
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    for i in range(size):
+        for z in range(size):
+            print("#", end="")
+        print()
