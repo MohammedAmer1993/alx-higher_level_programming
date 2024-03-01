9d315b0c5d6d0829a81f92d372a6cbe86aa829ea
Mohammed Amer <loard.amer.2010@gmail.com>
2024-03-01 04:30:29 +0200

    task 0 python network

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # bash file to git the content legnth of a file
 
-curl -si gazza.tech | grep Content-Length | sed 's/Content-Length: //g'
+curl -si "$1" | grep Content-Length | sed 's/Content-Length: //g'
