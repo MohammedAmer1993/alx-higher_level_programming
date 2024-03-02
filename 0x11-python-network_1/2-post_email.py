70aad787edd6e17cf5f4c5b57727e2138560f882
Mohammed Amer <loard.amer.2010@gmail.com>
2024-03-02 04:35:15 +0200

    task 2 proj python network#1

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
 """ module to sent an email in a url throug post method"""
+import urllib.request as req
+import urllib.parse as par
+import sys
 
 
 if __name__ == "__main__":
-    import urllib
-    import sys
+    values = {"email": sys.argv[2]}
+    url_parsed = par.urlencode(values).encode("utf-8")
+    with req.urlopen(sys.argv[1], data=url_parsed) as mysite:
+        d = mysite.read()
+        print(d.decode('utf-8'))
