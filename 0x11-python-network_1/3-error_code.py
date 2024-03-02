0c70604d9ecd220d3177fd8420264478549e973d
Mohammed Amer <loard.amer.2010@gmail.com>
2024-03-02 04:47:59 +0200

    task 3 proj python network#1

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python3
 """ module to send the req and show the return code"""
-
+import urllib.error as er
+import urllib.request as req
+import sys
 
 if __name__ == "__main__":
-    import urllib
-    import sys
+    try:
+        with req.urlopen(sys.argv[1]) as mysite:
+            data = mysite.read()
+            print(data.decode("utf-8"))
+    except er.URLError as e:
+        print(f"Error code: {e.code}")
+    except er.HTTPError as e:
+        print(f"Error code: {e.code}")
