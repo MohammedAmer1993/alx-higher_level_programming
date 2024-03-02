539b0fa027d84ba2ebc3e5f01aea2f5387d4abab
Mohammed Amer <loard.amer.2010@gmail.com>
2024-03-02 04:54:31 +0200

    task 4 proj python network#1

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 """ module to fetch status """
-
+import requests
 
 if __name__ == "__main__":
-    import requests
+    response = requests.get("https://alx-intranet.hbtn.io/status")
+    data = response.text
+    print("Body response:")
+    print(f"\t- type: {type(data)}")
+    print(f"\t- content: {data}")
