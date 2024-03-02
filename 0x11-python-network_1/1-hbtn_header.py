78f956a995c211c8b7a5b6e67a5549b642abe06b
Mohammed Amer <loard.amer.2010@gmail.com>
2024-03-02 04:17:47 +0200

    task 1 proj python network#1

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 """ fetch url and send request and get the value of var in header """
+import urllib.request as req
+import sys
 
 
 if __name__ == "__main__":
-    import urllib
-    import sys
+    with req.urlopen(sys.argv[1]) as mysite:
+        x = mysite.getheader("X-Request-Id")
+        print(x)
