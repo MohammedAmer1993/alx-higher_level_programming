d50911afe3db0b99da5eeb5b72a0bc06c76ff76f
Mohammed Amer <loard.amer.2010@gmail.com>
2024-03-02 04:58:38 +0200

    task 5 proj python network#1

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """ module to send req, get X req id and print """
-
+import requests
+import sys
 
 if __name__ == "__main__":
-    import requests
-    import sys
+    response = requests.get(sys.argv[1])
+    cont = response.headers.get("X-Request-Id")
+    print(cont)
