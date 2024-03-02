8c690a49d13c1ed1e2a298f3099bb0f508ce30eb
Mohammed Amer <loard.amer.2010@gmail.com>
2024-03-02 05:19:19 +0200

    task 7 proj python network#1

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
 """ take url, send req, show return """
-
+import requests
+import sys
 
 if __name__ == "__main__":
-    import requests
-    import sys
+    try:
+        req = requests.get(sys.argv[1])
+        req.raise_for_status()
+        print(req.text)
+    except requests.exceptions.RequestException as e:
+        print(f"Error code: {e.response.status_code}")
