3be8a0d4632939ffdc0eb863648b2d3e99d19180
Mohammed Amer <loard.amer.2010@gmail.com>
2024-03-03 05:25:27 +0200

    task 6 test driven python

---
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+import unittest
+
+max_integer = __import__("6-max_integer").max_integer
+
+
+class testmaxint(unittest.TestCase):
+
+    def test_none(self):
+        self.assertIsNone(max_integer([]))
+
+    def test_type(self):
+        self.assertRaises(TypeError, max_integer, 7)
+
+    def test_num(self):
+        self.assertEqual(max_integer([2, 8, 7, 6, 3]), 8)
+
+    def test_num2(self):
+        self.assertEqual(max_integer([2, -8, 7, -6, 3]), 7)
+
+    def test_num3(self):
+        self.assertEqual(max_integer([2.32, -8, 5.14, -66, 3]), 5.14)
+
+    def test_elem(self):
+        self.assertRaises(TypeError, max_integer, [2, "vo", True, [3]])
+
+
+if __name__ == "__main__":
+    unittest.main()
