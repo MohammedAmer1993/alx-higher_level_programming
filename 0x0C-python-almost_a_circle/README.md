7b1ef3ba878ef2a323eeac31c2af76070dfaed2a
Mohammed Amer <loard.amer.2010@gmail.com>
2024-03-12 13:31:23 +0200

    task 0 almost a cricle

---
+++ 0x0C-python-almost_a_circle/README.md
@@ -0,0 +1,1 @@
+this is readme file for almost a circle project
---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Module for base class for other shapes to inherit from"""
+
+
+class Base:
+    """the base class of inheratance"""
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """constructor of base class
+
+        Args:
+            id (int): this is the uniqie id of each obj
+        """
+        if id is None:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+        else:
+            self.id = id
+            Base.__nb_objects += 1
