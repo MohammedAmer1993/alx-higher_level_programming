8795ab4eeab7411ab3849b0dccdd713230716dd5
Mohammed Amer <loard.amer.2010@gmail.com>
2024-03-12 13:42:23 +0200

    task 0 almost a cricle

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,2 +1,16 @@
 #!/usr/bin/python3
 """test module for base class"""
+
+import unittest
+from models import base
+
+
+class test_base(unittest.TestCase):
+
+    def test_id(self):
+        testobj = base(32)
+        self.assertEqual(testobj.id, 32)
+
+
+if __name__ == "__main__":
+    unittest.main()
