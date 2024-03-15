66662832c2f31e3ec873c850bb9a84ed4a1b2232
Mohammed Amer <loard.amer.2010@gmail.com>
2024-03-16 00:23:42 +0200

    task 18 almost a circle

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -76,5 +76,4 @@ class Base:
         Return: the instance of the class which inherited from base
         """
         dummy = cls(**dictionary)
-        # dummy.update(**dictionary)
         return dummy
