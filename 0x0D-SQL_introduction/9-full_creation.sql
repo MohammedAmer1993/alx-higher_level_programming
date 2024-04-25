6695cd56c66d784a3cb8c97b0f8aa7ec72dc79c8
Mohammed Amer <loard.amer.2010@gmail.com>
2024-04-25 19:22:50 +0200

    task 9 sql

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,16 +1,14 @@
 --creates a table second_table
--- creation of table
+
+-- Create table
 CREATE TABLE IF NOT EXISTS second_table (
     id INT,
     name VARCHAR(256),
     score INT
 );
 
--- insertion in table
+-- Create records
 INSERT INTO second_table (id, name, score) VALUES (1, 'John', 10);
--- insertion in table
 INSERT INTO second_table (id, name, score) VALUES (2, 'Alex', 3);
--- insertion in table
 INSERT INTO second_table (id, name, score) VALUES (3, 'Bob', 14);
--- insertion in table
 INSERT INTO second_table (id, name, score) VALUES (4, 'George', 8);
