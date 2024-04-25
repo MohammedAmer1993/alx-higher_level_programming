b7c00b3afbf35359cd219a328ad9c33e7fea1755
Mohammed Amer <loard.amer.2010@gmail.com>
2024-04-25 19:20:07 +0200

    task 9 sql

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,6 +1,11 @@
 --creates a table second_table
 -- creation of table
-CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
+CREATE TABLE IF NOT EXISTS second_table (
+    id INT,
+    name VARCHAR(256),
+    score INT
+);
+
 -- insertion in table
 INSERT INTO second_table (id, name, score) VALUES (1, 'John', 10);
 -- insertion in table
