c5cb5c3763811d8c53d5489b7b6f89be1c0a7688
Mohammed Amer <loard.amer.2010@gmail.com>
2024-04-25 18:52:56 +0200

    sql intro again

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,5 +1,5 @@
 --creates a table second_table
-
-CREATE TABLE second_table (id INT, name VARCHAR(256), score INT);
-
+-- creation of table
+CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
+-- insertion in table
 INSERT INTO second_table VALUES (1, 'John', 10), (2, 'Alex', 3), (3, 'Bob', 14), (4, 'George', 8);
