e2ef5badbffd93797e2e2f47813d8eae12398948
Mohammed Amer <loard.amer.2010@gmail.com>
2024-04-26 15:41:25 +0300

    task 2 mysql more quiers

---
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,6 @@
+-- create a new user
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+-- GRANT PIVILIGES
+GRANT ALL ON *.* TO 'user_0d_1'@'localhost';
+-- FLUSH
+FLUSH PRIVILEGES;
