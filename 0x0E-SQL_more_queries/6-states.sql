8e3022e4d04bfa05a70a8b35968d002b67941974
Mohammed Amer <loard.amer.2010@gmail.com>
2024-04-26 18:02:45 +0300

    task 6 more quieris

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,10 +1,8 @@
 -- create database
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 
-
-
 -- create table
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
-    id INT UNIQUE NOT NULL PRIMARY KEY AOUT_INCREMENT,
+    id INT UNIQUE NOT NULL PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(256) NOT NULL
 );
