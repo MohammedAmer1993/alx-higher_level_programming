295d669452d49d65b2c0898d27aa35e41c4941dd
Mohammed Amer <loard.amer.2010@gmail.com>
2024-04-26 17:49:17 +0300

    task 100 sql intro

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,5 +1,11 @@
--- select database
+-- create database
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
+-- use database
 USE hbtn_0c_0;
+-- create table
+CREATE TABLE IF NOT EXISTS first_table (
+    name VARCHAR(256)
+);
 -- change every thing into utf8
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 -- edit table 
