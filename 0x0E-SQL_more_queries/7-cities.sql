a30a50437d15ba93c097b13a666cb9d925f98ac6
Mohammed Amer <loard.amer.2010@gmail.com>
2024-04-26 18:06:59 +0300

    task 7 more quieris

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -6,5 +6,5 @@ CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
     id INT UNIQUE NOT NULL PRIMARY KEY AUTO_INCREMENT,
     state_id INT NOT NULL,
     name VARCHAR(256) NOT NULL,
-    FOREIGN KEY(state_id) REFERENCES state(id)
+    FOREIGN KEY(state_id) REFERENCES states(id)
 );
