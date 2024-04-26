ab712f05d3f6bc47e2c47e4b2b24ecb8249946c0
Mohammed Amer <loard.amer.2010@gmail.com>
2024-04-26 04:38:07 +0300

    advanced mysql intr project 100, 101, 102, 103

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,5 +1,5 @@
 -- top 3 in aug and jul
-SELECT city, AVG(vv) AS avg_temp FROM 
+SELECT city, ROUND(AVG(vv), 4) AS avg_temp FROM 
 (SELECT city , MONTH, ROUND(AVG(value), 4) AS vv FROM temperatures GROUP BY city, month HAVING month = 7 OR month = 8) AS subqu 
 GROUP BY city 
 ORDER BY avg_temp DESC 
