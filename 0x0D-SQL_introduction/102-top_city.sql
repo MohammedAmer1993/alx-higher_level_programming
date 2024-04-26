be7ee8d14a3d892a684456b9ff15ceafe096c378
Mohammed Amer <loard.amer.2010@gmail.com>
2024-04-26 05:02:58 +0300

    advanced mysql intr project 100, 101, 102, 103

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,6 +1,10 @@
 -- top 3 in aug and jul
-SELECT city, ROUND(AVG(vv), 4) AS avg_temp FROM 
-(SELECT city , MONTH, ROUND(AVG(value), 5) AS vv FROM temperatures GROUP BY city, month HAVING month = 7 OR month = 8) AS subqu 
+CREATE TABLE IF NOT EXISTS temp
+    SELECT *
+    FROM temperatures
+    WHERE month = 7 OR month = 8;
+SELECT city, AVG(value) AS avg_temp
+FROM temp
 GROUP BY city 
 ORDER BY avg_temp DESC 
 LIMIT 3 ;
