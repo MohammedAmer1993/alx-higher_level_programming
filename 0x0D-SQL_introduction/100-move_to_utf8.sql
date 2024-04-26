1f81c167090e412e607a38765b478aafdb538d24
Mohammed Amer <loard.amer.2010@gmail.com>
2024-04-26 04:24:47 +0300

    advanced mysql intr project 100, 101, 102, 103

---
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,6 @@
+-- change every thing into utf8
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+-- edit table 
+ALTER TABLE CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+-- alter column
+ALTER TABLE first_table MODIFY COLUMN name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
