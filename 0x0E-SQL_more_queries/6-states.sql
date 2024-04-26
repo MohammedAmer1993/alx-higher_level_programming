cb06010fb6b8fb00be377c91e28c1ae0d49ec2c9
Mohammed Amer <loard.amer.2010@gmail.com>
2024-04-26 18:01:29 +0300

    task 6 more quieris

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,11 +1,10 @@
 -- create database
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 
--- select database
-USE hbtn_0d_usa;
+
 
 -- create table
-CREATE TABLE IF NOT EXISTS states (
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
     id INT UNIQUE NOT NULL PRIMARY KEY AOUT_INCREMENT,
     name VARCHAR(256) NOT NULL
 );
