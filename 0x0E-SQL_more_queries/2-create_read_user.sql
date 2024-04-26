35cf4b26d9fe5ae242365176c863fc3fbf1e4d89
Mohammed Amer <loard.amer.2010@gmail.com>
2024-04-26 16:12:18 +0300

    task 3 mysql more quiers

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,7 +1,7 @@
 --CREATE A NEW DATABASE
 CREATE DATABASE hbtn_0d_2;
 --CREATE NEW USER 
-CREATE USER IF NOT EXISTS `user_0d_2`@`loacalhost` IDENTIFIED BY 'user_0d_2_pwd';
+CREATE USER `user_0d_2`@`loacalhost` IDENTIFIED BY 'user_0d_2_pwd';
 --GRANT USAGE
 GRANT USAGE ON *.* TO `user_0d_2`@`loacalhost`;
 --GRANT SELECT PRIVILEGE
