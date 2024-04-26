f87212a3e68b41a13f8f9094ae7390b82b7c288e
Mohammed Amer <loard.amer.2010@gmail.com>
2024-04-26 16:32:13 +0300

    task 9 should work

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,4 +1,4 @@
---creates a table second_table
+-- creates a table second_table
 CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
 
 -- Script should create maximum of 4 records
