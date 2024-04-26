3b1c74705ec8a728ceb9eb12e9dec1a055c678a8
Mohammed Amer <loard.amer.2010@gmail.com>
2024-04-26 05:07:41 +0300

    advanced mysql intr project 100, 101, 102, 103

--- 0x0D-SQL_introduction/103-max_state.sql
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -1,3 +1,6 @@
 -- Max temp of a state
 
-SELECT state, MAX(value) AS max_temp FROM temperatures GROUP BY state ORDER BY state DES;
+SELECT state, MAX(value) AS max_temp
+FROM temperatures
+GROUP BY state
+ORDER BY max_temp DES;
