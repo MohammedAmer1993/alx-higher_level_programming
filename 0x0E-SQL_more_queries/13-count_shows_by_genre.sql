fcea8ceb6a9fd65d7556123f19d04c1dcb19ebf0
Mohammed Amer <loard.amer.2010@gmail.com>
2024-04-27 21:35:19 +0300

    task 13 more quieris

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,4 +1,4 @@
--- select from a table which is a result of a quary
+-- read from a table which is a result of joining two table
 
 SELECT tv_genres.name AS genre, COUNT(tv_show_genres.show_id) AS number
 FROM tv_genres
@@ -6,33 +6,3 @@ INNER JOIN tv_show_genres
 ON tv_genres.id = tv_show_genres.genre_id
 GROUP BY genre
 ORDER BY number DESC;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-SELECT b.name AS genre, COUNT(b.show_id) AS number FROM  
-(SELECT tv_genres.name, tv_show_genres.genre_id, tv_show_genres.show_id FROM tv_show_genres 
- INNER JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id) AS b 
- GROUP BY genre 
- ORDER BY number DESC;
-*/
-
