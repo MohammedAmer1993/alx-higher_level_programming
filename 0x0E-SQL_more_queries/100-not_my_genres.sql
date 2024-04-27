969fc5c51dafb6d613b5655517b90b404f7c69f8
Mohammed Amer <loard.amer.2010@gmail.com>
2024-04-28 01:52:12 +0300

    task 100 adv sql more queris

---
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -0,0 +1,9 @@
+-- not my genres
+
+SELECT name FROM tv_genres
+WHERE name NOT IN
+  (SELECT name FROM tv_genres
+  INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id 
+  INNER JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+  WHERE title = "Dexter")
+ORDER BY name;
