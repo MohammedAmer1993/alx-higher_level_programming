924984cf063a72060174efad42ab1b0a4bed5785
Mohammed Amer <loard.amer.2010@gmail.com>
2024-04-28 03:27:28 +0300

    task 100 adv sql more queris

---
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -0,0 +1,10 @@
+-- don't show any comedy tonight
+SELECT title FROM tv_shows
+WHERE title NOT IN
+(SELECT tv_shows.title FROM tv_shows
+ INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+ INNER JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+ WHERE tv_genres.name = "Comedy")
+ORDER BY title ASC;
+
+
