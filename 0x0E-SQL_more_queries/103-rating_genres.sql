07915f6b7d2854e0f2e8f580d2e9c8ce0f1ea06b
Mohammed Amer <loard.amer.2010@gmail.com>
2024-04-28 03:56:46 +0300

    task 103 adv sql more queris

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -1,7 +1,9 @@
 -- genres by ratings
 
-SELECT name , sum(rate) as rating from tv_genres
-INNER JOIN tv_show_genres on tv_genres.id = tv_show_genres.genre_id
-INNER JOIN tv_shows on tv_shows.id = tv_show_genres.show_id
-INNER JOIN tv_show_ratings on tv_shows.id = tv_show_ratings.show_id
-group by name;
+SELECT name , sum(rate) AS rating FROM tv_genres
+INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+INNER JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
+INNER JOIN tv_show_ratings ON tv_shows.id = tv_show_ratings.show_id
+GROUP BY name
+ORDER BY rating DESC;
+
